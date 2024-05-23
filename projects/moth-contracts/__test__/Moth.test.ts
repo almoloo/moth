3a1da480f0a1b56679706a3520d2d8fc09e1a03f
Hossein Arabi <73540115+Hossein-79@users.noreply.github.com>
2024-05-23 20:55:02 +0000

    working on profile

--- projects/moth-contracts/__test__/Moth.test.ts
+++ projects/moth-contracts/__test__/Moth.test.ts
@@ -1,5 +1,7 @@
 import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import algosdk from 'algosdk';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import { algos, getOrCreateKmdWalletAccount } from '@algorandfoundation/algokit-utils';
 import * as algokit from '@algorandfoundation/algokit-utils';
 import { MothClient } from '../contracts/clients/MothClient';
 
@@ -12,12 +14,23 @@ const defultPercentage: number = 5;
 const siteFee: number = 1;
 
 describe('HelloWorld', () => {
+  let sender: algosdk.Account;
+
   beforeEach(fixture.beforeEach);
 
   beforeAll(async () => {
     await fixture.beforeEach();
-    const { testAccount } = fixture.context;
+    const { testAccount, kmd, algod } = fixture.context;
     const { algorand } = fixture;
+
+    sender = await getOrCreateKmdWalletAccount(
+      {
+        name: 'tealscript-dao-sender',
+        fundWith: algos(100),
+      },
+      algod,
+      kmd
+    );
 
     appClient = new MothClient(
       {
@@ -29,11 +42,28 @@ describe('HelloWorld', () => {
     );
 
     await appClient.create.createApplication({ defultPercentage, siteFee });
-  });
 
-  test('get Site Fee', async () => {
-    const getSiteFee = await appClient.getSiteFee({});
+    test('get Site Fee', async () => {
+      const getSiteFee = await appClient.getSiteFee({});
 
-    expect(getSiteFee.return?.valueOf()).toEqual(BigInt(siteFee));
+      expect(getSiteFee.return?.valueOf()).toEqual(BigInt(siteFee));
+    });
+
+    test('getmbr', async () => {
+      const { appAddress } = await appClient.appClient.getAppReference();
+
+      const boxMBRPayment = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+        from: sender.addr,
+        to: appAddress,
+        amount: 100000,
+        suggestedParams: await algokit.getTransactionParams(undefined, algod),
+      });
+
+      const proposalFromMethod = await appClient.getMbr(
+        { boxMBRPayment },
+        { sender, boxes: [algosdk.decodeAddress(sender.addr).publicKey] }
+      );
+      expect(proposalFromMethod.return?.valueOf()).toBe(BigInt(24_900));
+    });
   });
 });
--- projects/moth-contracts/contracts/Moth.algo.ts
+++ projects/moth-contracts/contracts/Moth.algo.ts
@@ -1,7 +1,7 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
 type profile = {
-  titel: string;
+  title: string;
   logo: string;
   description: string;
   url: string;
@@ -12,14 +12,66 @@ type profile = {
 export class Moth extends Contract {
   defultPercentage = GlobalStateKey<uint64>();
 
+  // in uAlgo
   siteFee = GlobalStateKey<uint64>();
+
+  profiles = BoxMap<Account, profile>();
 
   createApplication(defultPercentage: uint64, siteFee: uint64): void {
     this.defultPercentage.value = defultPercentage;
     this.siteFee.value = siteFee;
   }
 
+  // eslint-disable-next-line no-unused-vars
+  getMBR(boxMBRPayment: PayTxn): number {
+    const preAppMBR = this.app.address.minBalance;
+    this.profiles(this.txn.sender).value = {
+      title: 'string',
+      logo: 'string',
+      description: 'string',
+      url: 'string',
+      loyaltyEnabled: true,
+      loyaltyPercentage: 10,
+    };
+    const Mbr = this.app.address.minBalance - preAppMBR;
+    this.profiles(this.txn.sender).delete();
+    return Mbr;
+  }
+
+  EditProfile(
+    boxMBRPayment: PayTxn,
+    title: string,
+    logo: string,
+    description: string,
+    url: string,
+    loyaltyEnabled: boolean,
+    loyaltyPercentage: uint64
+  ): profile {
+    const preAppMBR = this.app.address.minBalance;
+    this.profiles(this.txn.sender).value = {
+      title: title,
+      logo: logo,
+      description: description,
+      url: url,
+      loyaltyEnabled: loyaltyEnabled,
+      loyaltyPercentage: loyaltyPercentage,
+    };
+
+    verifyTxn(boxMBRPayment, {
+      receiver: this.app.address,
+      amount: this.app.address.minBalance - preAppMBR,
+    });
+
+    return this.profiles(this.txn.sender).value;
+  }
+
   GetSiteFee(): uint64 {
     return this.siteFee.value;
+  }
+
+  getProfile(address: Address): profile {
+    const values = this.profiles(address).value;
+
+    return values;
   }
 }
